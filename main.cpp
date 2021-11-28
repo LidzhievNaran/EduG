e1b4606b2710024c105ce4ba26abe3ef2dd49988
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-11-29 00:08:15 +0300

    push_front & push_back

--- main.cpp
+++ main.cpp
@@ -4,21 +4,17 @@
 using namespace std;
 #include <time.h>
 int main() {
-    clock_t start, end;
-    start = clock();
-    deque<int> buffer;
-    for (int i =0; i < 1000; i ++) {
-        for (int j = 1; j < 1000; j++) {
-            buffer.push_front(j);
-        }
-        do {
-            buffer.pop_back();
-        } while (buffer.size());
+    deque<int> arr = {1,2,3,4,5};
+    arr.push_front(6);
+    for (int i =0; i < arr.size(); i ++){
+        cout << arr[i]<< endl;
     }
-    end = clock();
-    cout << start<< ":" << end<< endl;
-
+    arr.pop_front();
+    for (int i =0; i < arr.size(); i ++){
+        cout << arr[i]<< endl;
+    }
 }
+
 
 
 
