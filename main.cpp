ab5ad4f9afcbc83f4939ba39865599360c6e91e6
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-11-29 00:42:58 +0300

    splice2

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <list>
 using namespace std;
 int main() {
-    list<int> source = {0,1,2,3};
+    list<int> source = {0,1,2,3,2,4,3,6,36};
     list<int>::iterator source_begin = source.begin()++;
-    list<int> target = {100,200};
+    list<int>::iterator source_end = source.end();
+    list<int> target ;
     list<int>::iterator pos = target.end();
-    target.splice(pos, source);
+    target.splice(pos, source, source_begin, source_end);//перемещаем все элементы source  в  target
     for (pos = target.begin(); pos!= target.end(); pos++){
         cout << *pos << endl;
     }
