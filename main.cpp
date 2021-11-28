f97061ea20bbd7fccf999645c7c8f779a660d9b4
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-11-29 00:59:29 +0300

    set

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 
 #include <iostream>
-#include <list>
+#include <set>
 using namespace std;
-int main() {
-    list<int> source = {0,1,2,3,2,4,3,6,36};
-    list<int>::iterator source_begin = source.begin()++;
-    list<int>::iterator source_end = source.end();
-    list<int> target ;
-    list<int>::iterator pos = target.end();
-    target.splice(pos, source, source_begin, source_end);//перемещаем все элементы source  в  target
-    for (pos = target.begin(); pos!= target.end(); pos++){
-        cout << *pos << endl;
+void show(set<int> &count){
+    set<int>::iterator iter;
+    for (iter = count.begin(); iter!= count.end(); ++iter){
+        cout << *iter<< " - ";
     }
+    cout << endl;
 }
+int main() {
+    set<int> count;
+    int new_number;
+    srand(0);
+    for (int i = 0;i < 10; i++){
+        new_number = rand()%9 +1;
+        count.insert(new_number);
+        cout << new_number << " - ";
+    }
+    cout << endl;
+    show(count);
+    count.erase(4);
+    show(count);
+}
+
 
 
 
