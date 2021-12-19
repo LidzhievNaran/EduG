00bb1fa969587ec988ea8bb6ea1e74839bae6576
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-12-20 01:04:49 +0300

    file out

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
 
 #include <iostream>
-#include <set>
+#include <fstream>
 using namespace std;
-void show(set<int> &count){
-    set<int>::iterator iter;
-    for (iter = count.begin(); iter!= count.end(); ++iter){
-        cout << *iter<< " - ";
-    }
-    cout << endl;
-}
-//&
 int main() {
-    set<int> count;
-    int new_number;
-    srand(0);
-    for (int i = 0;i < 10; i++){
-        new_number = rand()%9 +1;
-        count.insert(new_number);
-        cout << new_number << " - ";
-    }
-    cout << endl;
-    show(count);
-    count.erase(4);
-    show(count);
+    long a = 183477256783;
+    fstream f;
+    f.open("test.dat", ios::out);
+    f << "этот код записывается в файл" << endl;
+    f.close();
+    f.open("test.dat", ios::app);
+    f << "i wanna be the boshy " << a << endl;
+
+    f.close();
 }
-
-
-
 
 
 
